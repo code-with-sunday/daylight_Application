299c10535d60d3f6f7f12beaaa8c713288b8c6a4
sundaypeter <sunday.peter@me-cash.com>
2025-10-09 18:55:28 +0100

    first commit

---
+++ src/main/java/com/project/daylightAcademy/DaylightAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.project.daylightAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaylightAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaylightAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/daylightAcademy/DaylightAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.daylightAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaylightAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
